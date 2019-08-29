b08c1ea141436015561017fa90b116be6a7730b1
mhdiiilham <54275113+mhdiiilham@users.noreply.github.com>
2019-08-29 08:49:21 +0700

    exercise-5

--- exercise-5.js
+++ exercise-5.js
@@ -44,3 +44,21 @@ console.log('Fifth Word: ' +fifthWord)
 
 
 // 4. Breaking Sentence (yet Again) and Count Each Length
+var word = 'wow JavaScript is so cool';
+var firstWord = word.substring(0, 3);
+var secondWord = word.substring(4, 14);
+var thirdWord = word.substring(15,17);
+var fourthWord = word.substring(18, 20);
+var fifthWord = word.substring(21, 25);
+
+var firstWorldLength = firstWord.length;
+var secondWordLength = secondWord.length;
+var thirdWordLength = thirdWord.length;
+var fourthWordLength = fourthWord.length;
+var fifthWordLength = fifthWord.length;
+
+console.log('First Word: ' + firstWord + ', with length: ' + firstWorldLength);
+console.log('Second Word: ' + secondWord + ', with length: ' + secondWordLength)
+console.log('Third Word: ' + thirdWord + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord + ', with length: ' + fifthWordLength);
