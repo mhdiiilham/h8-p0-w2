30b8112c98becc465f83a755e656695db78fd30c
mhdiiilham <54275113+mhdiiilham@users.noreply.github.com>
2019-08-30 15:56:58 +0700

    fokusgit add .

--- tes.js
+++
@@ -1,8 +0,0 @@
-fun
-  
-  // TEST CASES
-  console.log(xo('xoxoxo')); // true
-  console.log(xo('oxooxo')); // false
-  console.log(xo('oxo')); // false
-  console.log(xo('xxxooo')); // true
-  console.log(xo('xoxooxxo')); // true
